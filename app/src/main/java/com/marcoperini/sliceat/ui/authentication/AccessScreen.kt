36d86ca880a74764c398f07c2b8eb13c30875ef5
giovanni <giovanni.vitiello10@gmail.com>
2020-04-14 16:46:48 +0200

    Complete AccessScreen

--- app/src/main/java/com/marcoperini/sliceat/ui/authentication/AccessScreen.kt
+++ app/src/main/java/com/marcoperini/sliceat/ui/authentication/AccessScreen.kt
@@ -2,11 +2,18 @@ package com.marcoperini.sliceat.ui.authentication
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.marcoperini.sliceat.R
+import com.marcoperini.sliceat.ui.Navigator
+import org.koin.android.ext.android.inject
 
 class AccessScreen : AppCompatActivity() {
+
+    private lateinit var backButton: Button
+
+    private val navigator: Navigator by inject()
 
     companion object {
         fun getIntent (startingActivityContext: Context) = Intent(startingActivityContext, AccessScreen::class.java)
@@ -16,5 +23,17 @@ class AccessScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.access_screen)
 
+        setupView()
+        setOnClickListener()
+    }
+
+    private fun setupView() {
+        backButton = findViewById(R.id.backButton)
+    }
+
+    private fun setOnClickListener() {
+        backButton.setOnClickListener {
+            navigator.goToAuthenticationScreen()
+        }
     }
 }
