49a59bbc5ff2ff6f97126237a4bb1724146eba8a
Giovanni Vitiello <giovanni.vitiello10@gmail.com>
2020-08-06 11:44:58 +0200

    Changed onboarding

--- app/src/main/java/com/marcoperini/sliceat/ui/onboarding/OnboardingAdapter.kt
+++ app/src/main/java/com/marcoperini/sliceat/ui/onboarding/OnboardingAdapter.kt
@@ -1,9 +1,14 @@
 package com.marcoperini.sliceat.ui.onboarding
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.marcoperini.sliceat.R
 import kotlinx.android.synthetic.main.item_page.view.ivImage
@@ -20,21 +25,41 @@ class OnboardingAdapter(val context: Context) : RecyclerView.Adapter<PagerVH>() 
 
     //binding the screen with view
     override fun onBindViewHolder(holder: PagerVH, position: Int) = holder.itemView.run {
+        val backgroundImage = BitmapFactory.decodeResource(resources, R.drawable.onboarding_background)
+
         if (position == 0) {
             tvTitle.text = context.getString(R.string.onboardingTitle1)
             tvAbout.text = context.getString(R.string.onboardingText1)
-            ivImage.setImageResource(R.drawable.undraw_map)
+
+            val firstImage = BitmapFactory.decodeResource(resources, R.drawable.onboarding_first)
+            val mergedImages = createSingleImageFromMultipleImages(firstImage, backgroundImage)
+
+            ivImage.setImageBitmap(mergedImages)
         }
         if (position == 1) {
             tvTitle.text = context.getString(R.string.onboardingTitle2)
             tvAbout.text = context.getString(R.string.onboardingText2)
-            ivImage.setImageResource(R.drawable.undraw_eating_together)
+            val firstImage = BitmapFactory.decodeResource(resources, R.drawable.onboarding_second)
+            val mergedImages = createSingleImageFromMultipleImages(firstImage, backgroundImage)
+
+            ivImage.setImageBitmap(mergedImages)
         }
         if (position == 2) {
             tvTitle.text = context.getString(R.string.onboardingTitle3)
             tvAbout.text = context.getString(R.string.onboardingText3)
-            ivImage.setImageResource(R.drawable.undraw_eating_together)
+            val firstImage = BitmapFactory.decodeResource(resources, R.drawable.onboarding_third)
+            val mergedImages = createSingleImageFromMultipleImages(firstImage, backgroundImage)
+
+            ivImage.setImageBitmap(mergedImages)
         }
+    }
+
+    private fun createSingleImageFromMultipleImages(firstImage: Bitmap, secondImage: Bitmap): Bitmap? {
+        val result = Bitmap.createBitmap(firstImage.width, firstImage.height, firstImage.config)
+        val canvas = Canvas(result)
+        canvas.drawBitmap(firstImage, 0f, 0f, null)
+        canvas.drawBitmap(secondImage, 0f, 0f, null)
+        return result
     }
 }
 
