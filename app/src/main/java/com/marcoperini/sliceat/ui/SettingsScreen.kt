bbf50d1d8c6c462368d53cda0fdb8cc6a529bafe
Giovanni Vitiello <giovanni.vitiello10@gmail.com>
2020-08-08 17:29:22 +0200

    Added toolbar

---
+++ app/src/main/java/com/marcoperini/sliceat/ui/SettingsScreen.kt
@@ -0,0 +1,12 @@
+package com.marcoperini.sliceat.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marcoperini.sliceat.R
+
+class SettingsScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_screen)
+    }
+}
